f9fd1bb222435067e9581e8cf27d599f5e3bc6a4
김선민 <tjsals116@gmail.com>
2025-02-14 17:43:27 +0900

    ♻️ refactor with ts

--- index.ts
+++ index.ts
@@ -4,20 +4,20 @@ import '@tensorflow/tfjs-node';
 const 온도 = [20, 21, 22, 23];
 const 판매량 = [40, 42, 44, 46];
 
-const 원인 = tf.tensor(온도);
-const 결과 = tf.tensor(판매량);
+const 원인 = tf.tensor(온도).reshape([온도.length, 1]);
+const 결과 = tf.tensor(판매량).reshape([판매량.length, 1]);
 
-const X = tf.input({ shape: [1]});
+const X = tf.input({ shape: [1] });
 const Y = tf.layers.dense({ units: 1 }).apply(X);
-const model = tf.model({ inputs: X, outputs: Y })
+const model = tf.model({ inputs: X, outputs: Y as tf.SymbolicTensor });
 
 model.compile({
     optimizer: tf.train.adam(),
     loss: tf.losses.meanSquaredError,
 })
 
-model.fit(원인, 결과, { epochs: 90000 }).then((result) => {
-    const 예측한결과 = model.predict(원인);
+model.fit(원인, 결과, { epochs: 3000 }).then((result) => {
+    const 예측한결과 = model.predict(원인) as tf.Tensor;
     예측한결과.print();
     model.save('file://./model-1a'); 
 });
--- load.ts
+++ load.ts
@@ -4,6 +4,11 @@ import '@tensorflow/tfjs-node';
 console.time('load');
 
 await tf.loadLayersModel('file://./model-1a/model.json').then((model) => {
-    model.predict(tf.tensor([20, 50, 100, 150, 90000])).print();
+    const result = model.predict(tf.tensor([20, 50, 100, 150, 90000]));
+    if (Array.isArray(result)) {
+        result.forEach(t => t.print());
+    } else {
+        result.print();
+    }
     console.timeEnd('load');
 })
